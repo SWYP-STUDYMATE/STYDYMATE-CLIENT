a0e9691b4ee998eb0a8bf1267bd1ab7a4ecd530b
EnENseong <139357931+EnENseong@users.noreply.github.com>
2025-08-07 21:50:37 +0900

    Main 페이지 충돌 해결

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -66,38 +66,6 @@ export default function Main() {
   };
 
   return (
-    <div className="bg-[#fafafa] min-h-screen flex flex-col items-center justify-center">
-      <div className="w-[120px] h-[120px] rounded-full bg-[#e7e7e7] flex items-center justify-center overflow-hidden mb-6">
-        <img
-          src={profileImage || "/assets/basicProfilePic.png"}
-          alt="프로필"
-          className="object-cover w-full h-full"
-          onError={(e) => {
-            e.target.onerror = null; // 무한 루프 방지
-            e.target.src = "/assets/basicProfilePic.png";
-          }}
-        />
-      </div>
-      <h1 className="text-3xl font-bold mb-4">{nickname}님, 환영합니다!</h1>
-      <p className="mb-8">거주지&시간대: {residence}</p>
-      <p className="mb-8">
-        임시로 만든 메인페이지입니다. <br />
-        테스트 주구창창 여기다 다 될거임
-      </p>
-      <LogoutButton />
-      <TokenTest />
-      <ProgressBar total={100} value={50} />
-
-      <div className="mt-8 w-64">
-        <CommonButton text="채팅방으로 이동(테스트)" onClick={handleGoToChat} />
-      </div>
-
-      <CommonChecklistItem
-        label="취업/진학"
-        checked={true}
-        onChange={() => {}}
-      />
-
       <div className="bg-[#fafafa] min-h-screen flex flex-col">
         <MainHeader />
         <div className="flex flex-1 p-6 space-x-6 overflow-hidden">
